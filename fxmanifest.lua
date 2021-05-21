afaf374a72ee2cf474a3797e1e08ed2fb190cb29
Pablo (From server) <pabzapata16@gmail.com>
2021-05-21 14:40:15 +0200

    Init repo

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+version '1.0.0'
+author 'Pablo ZAPATA (pablo_1610)'
+description 'Gamemode working with es_extended for Los Narcos'
+repository 'https://github.com/PABLO-1610/narcos_gamemode'
+
+resource_type 'gametype' { name = 'Narcos RolePlay' }
+
+
+game 'common'
+fx_version 'adamant'

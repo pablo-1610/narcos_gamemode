fd42906f2f36a78d223558c73fe7fe52c365da81
Pablo <pablo.zapata.dev@gmail.com>
2021-05-27 21:16:34 +0200

    [Core] (client): Modifying utils

--- core/client/utils.lua
+++ core/client/utils.lua
@@ -13,13 +13,92 @@
 
 NarcosClient = {}
 
+-- MenuHelper
+NarcosClient.MenuHelper = {
+    alert = nil,
+
+    defineLabelString = function(valueToCheck)
+        if valueToCheck ~= nil and valueToCheck ~= "" then
+            return ("~g~%s"):format(valueToCheck)
+        else
+            return "~b~Définir ~s~→→"
+        end
+    end,
+
+    defineLabelInt = function(valueToCheck, currency)
+        if valueToCheck ~= nil then
+            return ("~g~%s%s"):format(valueToCheck,(currency or ""))
+        else
+            return "~b~Définir ~s~→→"
+        end
+    end
+}
+
+-- InputHelper
+NarcosClient.InputHelper = {
+    showBox = function(TextEntry, ExampleText, MaxStringLenght, isValueInt)
+        AddTextEntry('FMMC_KEY_TIP1', TextEntry)
+        DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", ExampleText, "", "", "", MaxStringLenght)
+        blockinput = true
+        while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
+            Wait(0)
+        end
+        if UpdateOnscreenKeyboard() ~= 2 then
+            local result = GetOnscreenKeyboardResult()
+            Wait(500)
+            blockinput = false
+            if isValueInt then
+                local isNumber = tonumber(result)
+                if isNumber then
+                    return result
+                else
+                    return nil
+                end
+            end
+
+            return result
+        else
+            Wait(500)
+            blockinput = false
+            return nil
+        end
+    end,
+
+    validateInputStringDefinition = function(valueToCheck)
+        return (valueToCheck ~= nil and valueToCheck ~= "")
+    end,
+
+    validateInputStringRegex = function(valueToCheck, regex)
+        return string.match(valueToCheck, regex)
+    end,
+
+    validateInputIntDefinition = function(valueToCheck, positive)
+        local checkPositivity = true
+        if positive then
+            if valueToCheck < 0 then
+                checkPositivity = false
+            end
+        end
+        return (valueToCheck ~= nil and checkPositivity)
+    end
+}
+
+-- Other
 NarcosClient.warnVariator = "~r~"
 NarcosClient.dangerVariator = "~y~"
 
 Narcos.newRepeatingTask(function()
-    if NarcosClient.warnVariator == "~r~" then NarcosClient.warnVariator = "~s~" else NarcosClient.warnVariator = "~r~" end
-    if NarcosClient.dangerVariator == "~y~" then NarcosClient.dangerVariator = "~o~" else NarcosClient.dangerVariator = "~y~" end
-end, nil, 0,650)
+    if NarcosClient.warnVariator == "~r~" then
+        NarcosClient.warnVariator = "~s~"
+    else
+        NarcosClient.warnVariator = "~r~"
+    end
+    if NarcosClient.dangerVariator == "~y~" then
+        NarcosClient.dangerVariator = "~o~"
+    else
+        NarcosClient.dangerVariator = "~y~"
+    end
+end, nil, 0, 650)
 
 NarcosClient.toServer = function(eventName, ...)
     TriggerServerEvent("narcos:" .. Narcos.hash(eventName), ...)
@@ -28,35 +107,13 @@ end
 NarcosClient.requestModel = function(model)
     model = GetHashKey(model)
     RequestModel(model)
-    while not HasModelLoaded(model) do Wait(1) end
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
 end
 
 NarcosClient.trace = function(message)
     print(("[^1Los Narcos^7] ^7%s"):format(message))
-end
-
-NarcosClient.inputBox = function(TextEntry, ExampleText, MaxStringLenght, isValueInt)
-    AddTextEntry('FMMC_KEY_TIP1', TextEntry)
-    DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", ExampleText, "", "", "", MaxStringLenght)
-    blockinput = true
-    while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
-        Wait(0)
-    end
-    if UpdateOnscreenKeyboard() ~= 2 then
-        local result = GetOnscreenKeyboardResult()
-        Wait(500)
-        blockinput = false
-        if isValueInt then
-            local isNumber = tonumber(result)
-            if isNumber then return result else return nil end
-        end
-
-        return result
-    else
-        Wait(500)
-        blockinput = false
-        return nil
-    end
 end
 
 NarcosClient.advancedNotification = function(sender, subject, msg, textureDict, iconType)
@@ -94,12 +151,22 @@ NarcosClient.freezePlayer = function(id, bool)
 end
 
 NarcosClient.playAnim = function(dict, anim, flag, blendin, blendout, playbackRate, duration)
-    if blendin == nil then blendin = 1.0 end
-    if blendout == nil then blendout = 1.0 end
-    if playbackRate == nil then playbackRate = 1.0 end
-    if duration == nil then duration = -1 end
+    if blendin == nil then
+        blendin = 1.0
+    end
+    if blendout == nil then
+        blendout = 1.0
+    end
+    if playbackRate == nil then
+        playbackRate = 1.0
+    end
+    if duration == nil then
+        duration = -1
+    end
     RequestAnimDict(dict)
-    while not HasAnimDictLoaded(dict) do Wait(1) end
+    while not HasAnimDictLoaded(dict) do
+        Wait(1)
+    end
     TaskPlayAnim(GetPlayerPed(-1), dict, anim, blendin, blendout, duration, flag, playbackRate, 0, 0, 0)
     RemoveAnimDict(dict)
 end

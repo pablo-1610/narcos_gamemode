9eec7cb6dbc99775f7fd9343626e4719fc57b995
Pablo <pablo.zapata.dev@gmail.com>
2021-06-14 14:40:44 +0200

    Update sqlcheme.sql

--- schemes/sqlcheme.sql
+++ schemes/sqlcheme.sql
@@ -1,65 +1,38 @@
-CREATE TABLE `items` (
-  `name` varchar(50) NOT NULL,
+CREATE TABLE `players` (
+  `id` int(11) NOT NULL COMMENT 'Id unique de l''utilisateur',
+  `lastInGameId` int(11) NOT NULL COMMENT 'Dernier id utilisé en jeu',
+  `license` varchar(80) NOT NULL COMMENT 'Licence RockStar associée au compte',
+  `rank` varchar(80) NOT NULL COMMENT 'Le rang du joueur',
+  `name` text NOT NULL COMMENT 'Pseudo du joueur (Steam ou PC)',
+  `body` text NOT NULL COMMENT 'Informations textures du joueur (tête, visage et corps), ceci exclu les tenues et autres accessoires de ped.',
+  `outfits` text NOT NULL COMMENT 'Tenues du joueurs',
+  `selectedOutfit` int(11) NOT NULL COMMENT 'Tenue actuelle du joueur',
+  `identity` text NOT NULL COMMENT 'Identité du joueur (Prénom, Nom, Age)',
+  `cash` int(11) NOT NULL COMMENT 'Argent (liquide) du joueur',
+  `position` text NOT NULL COMMENT 'La dernière position du joueur'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `ranks` (
+  `id` varchar(50) NOT NULL,
   `label` varchar(50) NOT NULL,
-  `weight` int(11) NOT NULL DEFAULT 1,
-  `rare` tinyint(4) NOT NULL DEFAULT 0,
-  `can_remove` tinyint(4) NOT NULL DEFAULT 1
+  `color` varchar(10) NOT NULL,
+  `permissions` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `jobs` (
-  `name` varchar(50) NOT NULL,
-  `label` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `ranks` (`id`, `label`, `color`, `permissions`) VALUES
+('admin', 'Admin', '^1', '[]'),
+('default', 'Joueur', '^7', '[]'),
+('helper', 'Assistant', '^7', '[]'),
+('moderator', 'Modérateur', '^7', '[]'),
+('moderator+', 'Modérateur+', '^7', '[]'),
+('owner', 'Propriétaire', '^7', '[]'),
+('vip', 'VIP', '^7', '[]'),
+('vip+', 'VIP+', '^7', '[]');
 
-INSERT INTO `jobs` (`name`, `label`) VALUES
-('unemployed', 'Sans emplois');
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`license`),
+  ADD UNIQUE KEY `id` (`id`);
 
-CREATE TABLE `job_grades` (
-  `id` int(11) NOT NULL,
-  `job_name` varchar(50) DEFAULT NULL,
-  `grade` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `label` varchar(50) NOT NULL,
-  `salary` int(11) NOT NULL,
-  `skin_male` longtext NOT NULL,
-  `skin_female` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
-(1, 'unemployed', 0, 'unemployed', 'Sans emplois', 200, '{}', '{}');
-
-CREATE TABLE `users` (
-  `identifier` varchar(40) NOT NULL,
-  `accounts` longtext DEFAULT NULL,
-  `group` varchar(50) DEFAULT 'user',
-  `inventory` longtext DEFAULT NULL,
-  `job` varchar(20) DEFAULT 'unemployed',
-  `job_grade` int(11) DEFAULT 0,
-  `loadout` longtext DEFAULT NULL,
-  `position` varchar(255) DEFAULT '{"x": 2614.53, "y": 2920.02, "z": 40.42, "heading": 57.27}',
-  `identity` text NOT NULL,
-  `baseCharacter` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `users` (`identifier`, `accounts`, `group`, `inventory`, `job`, `job_grade`, `loadout`, `position`, `identity`, `baseCharacter`) VALUES
-('8fc3f9bf5017c451d19593ae7d1105989d6635e0', '{\"bank\":50800,\"black_money\":0,\"money\":0}', 'user', '[]', 'unemployed', 0, '[]', '{\"x\":686.0,\"y\":577.3,\"heading\":14.5,\"z\":130.5}', '', '');
-
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`name`);
-
-ALTER TABLE `jobs`
-  ADD PRIMARY KEY (`name`);
-
-ALTER TABLE `job_grades`
+ALTER TABLE `ranks`
   ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`identifier`);
-
-ALTER TABLE `job_grades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

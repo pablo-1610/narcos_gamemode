9942e4e5e27d1ec8cd483c8fb645f7fc805ab6a2
Pablo <pablo.zapata.dev@gmail.com>
2021-06-14 14:49:43 +0200

    Add uuid generation method

--- core/shared/utils/events.lua
+++ core/shared/utils/events.lua
@@ -83,3 +83,15 @@ end
 Narcos.second = function(from)
     return from * 1000
 end
+
+---uuid
+---@public
+---@return string
+Narcos.uuid = function()
+    local random = math.random
+    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    return string.gsub(template, '[xy]', function (c)
+        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+        return string.format('%x', v)
+    end)
+end

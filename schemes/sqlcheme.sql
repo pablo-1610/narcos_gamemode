f52a3ba43cc2e0383c2a2edfb7e4b5142e2ccb55
Pablo <pablo.zapata.dev@gmail.com>
2021-06-15 17:31:26 +0200

    Update sqlscheme

--- schemes/sqlcheme.sql
+++ schemes/sqlcheme.sql
@@ -1,22 +1,22 @@
 CREATE TABLE `players` (
-  `id` int(11) NOT NULL COMMENT 'Id unique de l''utilisateur',
+  `id` int(11) NOT NULL COMMENT 'Id unique du joueur (utilisé pour la boutique)',
   `lastInGameId` int(11) NOT NULL COMMENT 'Dernier id utilisé en jeu',
-  `license` varchar(80) NOT NULL COMMENT 'Licence RockStar associée au compte',
+  `license` varchar(80) NOT NULL COMMENT 'Licence RockStar du joueur',
   `rank` varchar(80) NOT NULL COMMENT 'Le rang du joueur',
   `name` text NOT NULL COMMENT 'Pseudo du joueur (Steam ou PC)',
   `body` text NOT NULL COMMENT 'Informations textures du joueur (tête, visage et corps), ceci exclu les tenues et autres accessoires de ped.',
   `outfits` text NOT NULL COMMENT 'Tenues du joueurs',
-  `selectedOutfit` int(11) NOT NULL COMMENT 'Tenue actuelle du joueur',
+  `selectedOutfit` varchar(80) NOT NULL COMMENT 'Tenue actuelle du joueur',
   `identity` text NOT NULL COMMENT 'Identité du joueur (Prénom, Nom, Age)',
   `cash` int(11) NOT NULL COMMENT 'Argent (liquide) du joueur',
   `position` text NOT NULL COMMENT 'La dernière position du joueur'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `ranks` (
-  `id` varchar(50) NOT NULL,
-  `label` varchar(50) NOT NULL,
-  `color` varchar(10) NOT NULL,
-  `permissions` text NOT NULL
+  `id` varchar(50) NOT NULL COMMENT 'Id unique du grade',
+  `label` varchar(50) NOT NULL COMMENT 'Label (affichage) du grade',
+  `color` varchar(10) NOT NULL COMMENT 'Couleur du grade',
+  `permissions` text NOT NULL COMMENT 'Permissions du grade'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `ranks` (`id`, `label`, `color`, `permissions`) VALUES
@@ -30,9 +30,11 @@ INSERT INTO `ranks` (`id`, `label`, `color`, `permissions`) VALUES
 ('vip+', 'VIP+', '^7', '[]');
 
 ALTER TABLE `players`
-  ADD PRIMARY KEY (`license`),
-  ADD UNIQUE KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `ranks`
   ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `players`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id unique du joueur (utilisé pour la boutique)', AUTO_INCREMENT=11;
 COMMIT;

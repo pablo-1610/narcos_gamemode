92d315bfe9e34a2748f14e2e39916e2029105aaa
Pablo <pablo.zapata.dev@gmail.com>
2021-06-21 01:54:33 +0200

    add basics

---
+++ addons/basicneeds/client/main.lua
@@ -0,0 +1,16 @@
+---@author Pablo Z.
+---@version 1.0
+--[[
+  This file is part of Los Narcos.
+  
+  File [main] created at [21/06/2021 01:53]
+
+  Copyright (c) Los Narcos - All Rights Reserved
+
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+
+NarcosClient_InventoriesManager.registerUsable("bread", function()
+    print("ok !")
+end)

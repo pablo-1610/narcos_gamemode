e731281f80e1c41f1b202abdb22566ee22957efc
Pablo <pablo.zapata.dev@gmail.com>
2021-06-21 00:27:30 +0200

    Add GameState enum

---
+++ core/shared/utils/enums.lua
@@ -0,0 +1,19 @@
+---@author Pablo Z.
+---@version 1.0
+--[[
+  This file is part of Los Narcos.
+  
+  File [enums] created at [20/06/2021 23:44]
+
+  Copyright (c) Los Narcos - All Rights Reserved
+
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+
+NarcosEnums = {
+    GameStates = {
+        LOADING = 0,
+        PLAYING = 1
+    }
+}

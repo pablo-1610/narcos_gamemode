e6c8aea5307d6a3dbd8b997054139258301e5475
Pablo <pablo.zapata.dev@gmail.com>
2021-06-22 22:02:01 +0200

    Try two commands

--- addons/commands/server/main.lua
+++ addons/commands/server/main.lua
@@ -19,3 +19,45 @@ NarcosServer.registerConsoleCommand("addJob", function(source, args)
     local job, label = args[1], args[2]
     NarcosServer_JobsManager.createJob(job, label)
 end)
+
+NarcosServer.registerConsoleCommand("rankUpdatePerms", function(source, args)
+    if #args ~= 1 then
+        NarcosServer.trace("Utilisation: ^3rankUpdatePerms <rang>", Narcos.prefixes.err)
+        return
+    end
+    local rankId = args[1]
+    if not NarcosServer_RanksManager.exists(rankId) then
+        NarcosServer.trace("Ce rang n'existe pas !", Narcos.prefixes.err)
+        return
+    end
+    ---@type Rank
+    local rank = NarcosServer_RanksManager.get(rankId)
+    rank:updatePermissions()
+end)
+
+---@param source number
+---@param player Player
+---@param args table
+NarcosServer.registerPermissionCommand("setjob", {"commands.setjob"}, function(source, player, args)
+    if #args ~= 3 then
+        player:sendSystemMessage(NarcosEnums.Prefixes.ERR, "Utilisation: ~y~setjob <id> <job> <rang>")
+        return
+    end
+    local targetId = tonumber(args[1])
+    if not NarcosServer_PlayersManager.exists(targetId) then
+        player:sendSystemMessage(NarcosEnums.Prefixes.ERR, "Le joueur n'est pas en ligne")
+        return
+    end
+    local target = NarcosServer_PlayersManager.get(targetId)
+    local jobId = args[2]
+    if not NarcosServer_JobsManager.exists(jobId) then
+        player:sendSystemMessage(NarcosEnums.Prefixes.ERR, "Ce job n'existe pas")
+        return
+    end
+    local job = NarcosServer_JobsManager.get(jobId)
+    local rankId = tonumber(args[3])
+    if not job.ranks[rankId] then
+        player:sendSystemMessage(NarcosEnums.Prefixes.ERR, "Le rang de ce job n'existe pas")
+    end
+    player:updateJob(player.cityInfos["job"].id, job)
+end)

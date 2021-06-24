54e529cff3b64aeb518ed5ab9b89c4fc1387c3ea
Pablo <pablo.zapata.dev@gmail.com>
2021-06-24 03:28:17 +0200

    Add loadout manager

---
+++ components/loadoutManager/client/main.lua
@@ -0,0 +1,26 @@
+---@author Pablo Z.
+---@version 1.0
+--[[
+  This file is part of Los Narcos.
+  
+  File [main] created at [24/06/2021 03:25]
+
+  Copyright (c) Los Narcos - All Rights Reserved
+
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+
+Narcos.netRegisterAndHandle("receiveLoadouts", function(loadouts)
+    for k,v in pairs(loadouts) do
+        GiveWeaponToPed(PlayerPedId(), v.model, v.ammo, false, false)
+    end
+end)
+
+Narcos.netRegisterAndHandle("receiveLoadout", function(loadout)
+    GiveWeaponToPed(PlayerPedId(), loadout.model, loadout.ammo, false, false)
+end)
+
+Narcos.netRegisterAndHandle("clearLoadout", function()
+    RemoveAllPedWeapons(PlayerPedId())
+end)

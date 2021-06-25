4e2b850e3c0ef33cd64f74fe56f66596f5835fe1
Pablo <pablo.zapata.dev@gmail.com>
2021-06-25 15:55:13 +0200

    Add defaultJob

--- core/config/server.lua
+++ core/config/server.lua
@@ -15,6 +15,7 @@ NarcosConfig_Server = {
     populationEnabled = false,
 
     defaultRank = "default",
+    defaultJob = "unemployed",
 
     bannedInstance = 666,
 
@@ -26,8 +27,8 @@ NarcosConfig_Server = {
 
     unemployedJobName = "Explorateur",
 
-    baseCityInfos = {["job"] = {id = -1, rank = -1}, ["org"] = {id = -1, rank = -1}},
     baseInventory = {["bread"] = 10, ["water"] = 10},
+    baseCityInfos = {["job"] = {id = "unemployed", rank = 3}, ["org"] = {id = "unemployed", rank = 3}},
     baseBuilderMoney = 5000,
 
     errorWebhook = "https://discord.com/api/webhooks/856303231235260416/QCbp0wufoZb50RgL5aE9Hpu0R_h-u5VgyY2Jnj2Y049KUW9ErNl-jadAFvX4BWx-sVNI",

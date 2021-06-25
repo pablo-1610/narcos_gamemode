ddd2a2d29b3550c2da8f4ea97d5cf9896dc80984
Pablo <pablo.zapata.dev@gmail.com>
2021-06-25 15:23:02 +0200

    Modifying Job#zonesRelatives

--- components/jobsManager/server/objects/Job.lua
+++ components/jobsManager/server/objects/Job.lua
@@ -31,15 +31,20 @@ setmetatable(Job, {
         self.label = label
         self.money = money
         self.ranks = {}
-        for k,v in pairs(ranks) do
-           self.ranks[k] = JobRank(v.label, v.permissions)
+        for k, v in pairs(ranks) do
+            self.ranks[k] = JobRank(v.label, v.permissions)
         end
         self.positions = positions
         self.zonesRelatives = {}
         for k, v in pairs(self.positions) do
-            self.zonesRelatives[k] = NarcosServer_ZonesManager.createPrivate(v.location, 22, {r = 158, g = 245, b = 66, a = 255}, function(source)
-                self:interactWithZone(source, k)
+            self.zonesRelatives[k] = {}
+            self.zonesRelatives[k].perm = v.perm
+            self.zonesRelatives[k].zone = NarcosServer_ZonesManager.createPrivate(vector3(v.location.x, v.location.y, v.location.z), 20, { r = 255, g = 255, b = 255, a = 255 }, function(source)
+                self:interactWithBaseZone(source, k)
             end, ("Appuyez sur ~INPUT_CONTEXT~ pour intéragir avec %s"):format(v.desc), 20.0, 1.0)
+            if v.blip.active then
+                self.zonesRelatives[k].blip = NarcosServer_BlipsManager.createPrivate(vector3(v.location.x, v.location.y, v.location.z), v.blip.sprite, v.blip.color, NarcosConfig_Server.blipsScale, ("(%s) %s"):format(self.label, v.label), false)
+            end
         end
         self.type = type
         self.inventory = NarcosServer_InventoriesManager.getOrCreate(("job:%s"):format(self.name), ("Entrepôt %s"):format(label), 100.0, 2)
@@ -48,9 +53,50 @@ setmetatable(Job, {
     end
 })
 
----interactWithZone
+---handlePlayerJoined
 ---@public
 ---@return void
-function Job:interactWithZone(_src, zone)
+---@param _src number
+---@param player Player
+function Job:handlePlayerJoined(_src, player)
+    ---@type JobRank
+    local playerRank = self.ranks[player.cityInfos["job"].rank]
+    for zoneName, zoneData in pairs(self.zonesRelatives) do
+        if zoneData.perm ~= nil then
+            if playerRank:hasPermission(zoneData.perm) then
+                if zoneData.blip ~= nil then
+                    NarcosServer_BlipsManager.addAllowed(zoneData.blip, _src)
+                end
+                NarcosServer_ZonesManager.addAllowed(zoneData.zone, _src)
+            end
+        else
+            if zoneData.blip ~= nil then
+                NarcosServer_BlipsManager.addAllowed(zoneData.blip, _src)
+            end
+            NarcosServer_ZonesManager.addAllowed(zoneData.zone, _src)
+        end
+    end
+end
+
+function Job:handlePlayerLeft(_src, player)
+    local playerRank = self.ranks[player.cityInfos["job"].rank]
+    for zoneName, zoneData in pairs(self.zonesRelatives) do
+        if zoneData.perm ~= nil then
+            if playerRank:hasPermission(zoneData.perm) then
+                if zoneData.blip ~= nil then
+                    NarcosServer_BlipsManager.removeAllowed(zoneData.blip, _src)
+                end
+                NarcosServer_ZonesManager.removeAllowed(zoneData.zone, _src)
+            end
+        else
+            if zoneData.blip ~= nil then
+                NarcosServer_BlipsManager.removeAllowed(zoneData.blip, _src)
+            end
+            NarcosServer_ZonesManager.removeAllowed(zoneData.zone, _src)
+        end
+    end
+end
+
+function Job:interactWithBaseZone(source, zone)
 
 end

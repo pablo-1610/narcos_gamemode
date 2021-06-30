41b88b56d3de75f0d3ffb694c5b641c577f5d395
Pablo <pablo.zapata.dev@gmail.com>
2021-06-30 13:04:41 +0200

    Add police precise

---
+++ jobs/police/server/main.lua
@@ -0,0 +1,20 @@
+---@author Pablo Z.
+---@version 1.0
+--[[
+  This file is part of Los Narcos.
+  
+  File [main] created at [30/06/2021 12:35]
+
+  Copyright (c) Los Narcos - All Rights Reserved
+
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+
+Narcos.netHandle("jobsLoaded", function()
+    NarcosServer_JobsManager.precise["police"] = {
+        garageVehicles = {
+            ["fb2"] = {color = {80, 128, 74}}
+        }
+    }
+end)

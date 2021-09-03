6a1feed89a8db391c1a3bb68449ccf59a1e3fd59
Pablo <pablo.zapata.dev@gmail.com>
2021-09-04 00:09:22 +0200

    First steps component drawer

---
+++ components/componentDrawer/client/main.lua
@@ -0,0 +1,15 @@
+---@author Pablo Z.
+---@version 1.0
+--[[
+  This file is part of Los Narcos.
+  
+  File [main] created at [04/09/2021 00:08]
+
+  Copyright (c) Los Narcos - All Rights Reserved
+
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+
+NarcosClient_ComponentDrawer = {}
+

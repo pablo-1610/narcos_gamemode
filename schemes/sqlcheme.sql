a17370c3183c4e7aa240c83d43b861697fadba25
Pablo <pablo.zapata.dev@gmail.com>
2021-09-04 20:13:49 +0200

    Update SQLScheme

--- schemes/sqlcheme.sql
+++ schemes/sqlcheme.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : ven. 25 juin 2021 à 15:54
--- Version du serveur :  10.4.18-MariaDB
--- Version de PHP : 8.0.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `narcos`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `cards`
---
-
 CREATE TABLE `cards` (
   `id` int(11) NOT NULL,
   `owner` varchar(80) NOT NULL,
@@ -36,19 +7,6 @@ CREATE TABLE `cards` (
   `history` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `cards`
---
-
-INSERT INTO `cards` (`id`, `owner`, `number`, `pin`, `balance`, `history`) VALUES
-(23, 'license:8fc3f9bf5017c451d19593ae7d1105989d6635e0', '8728 7531 1422 9521', 1717, 0, '[{\"ammount\":150,\"desc\":\"Dépôt physique\",\"positive\":true,\"date\":\"le 06/24/2021 à 15:56 par Pablo BARILLAS\"},{\"ammount\":150,\"desc\":\"Retrait physique\",\"positive\":false,\"date\":\"le 06/24/2021 à 15:57 par Pablo BARILLAS\"}]');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `inventories`
---
-
 CREATE TABLE `inventories` (
   `identifier` varchar(80) NOT NULL COMMENT 'Identifiant unique de l''inventaire',
   `label` varchar(80) NOT NULL COMMENT 'Nom d''affichage de l''inventaire',
@@ -57,19 +15,6 @@ CREATE TABLE `inventories` (
   `content` text NOT NULL COMMENT 'Contenu de l''inventaire'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `inventories`
---
-
-INSERT INTO `inventories` (`identifier`, `label`, `capacity`, `type`, `content`) VALUES
-('job:unemployed', 'Entrepôt Explorateur', 100, 2, '[]');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `items`
---
-
 CREATE TABLE `items` (
   `name` varchar(80) NOT NULL COMMENT 'Identifiant unique de l''item',
   `label` varchar(80) NOT NULL COMMENT 'Nom d''affichage de l''item',
@@ -77,41 +22,19 @@ CREATE TABLE `items` (
   `vip` int(11) NOT NULL DEFAULT 0 COMMENT 'Détermine si l''objet est reservé aux VIPs'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `items`
---
-
 INSERT INTO `items` (`name`, `label`, `weight`, `vip`) VALUES
 ('bread', 'Pain', 0.2, 0),
 ('water', 'Bouteille d\'eau', 0.45, 0);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `jobs`
---
 
 CREATE TABLE `jobs` (
   `name` varchar(80) NOT NULL,
   `label` varchar(80) NOT NULL,
   `money` int(11) NOT NULL,
+  `history` text NOT NULL,
   `ranks` text NOT NULL,
   `positions` text NOT NULL,
   `type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `jobs`
---
-
-INSERT INTO `jobs` (`name`, `label`, `money`, `ranks`, `positions`, `type`) VALUES
-('unemployed', 'Explorateur', 0, '[]', '[]', 3);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `players`
---
 
 CREATE TABLE `players` (
   `id` int(11) NOT NULL COMMENT 'Id unique du joueur (utilisé pour la boutique)',
@@ -132,12 +55,6 @@ CREATE TABLE `players` (
   `params` text NOT NULL COMMENT 'Paramètres du joueur'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `ranks`
---
-
 CREATE TABLE `ranks` (
   `id` varchar(50) NOT NULL COMMENT 'Id unique du grade',
   `label` varchar(50) NOT NULL COMMENT 'Label (affichage) du grade',
@@ -145,77 +62,37 @@ CREATE TABLE `ranks` (
   `permissions` text NOT NULL COMMENT 'Permissions du grade'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `ranks`
---
-
 INSERT INTO `ranks` (`id`, `label`, `color`, `permissions`) VALUES
 ('admin', 'Admin', '^1', '[]'),
 ('default', 'Joueur', '^7', '[]'),
 ('helper', 'Assistant', '^7', '[]'),
 ('moderator', 'Modérateur', '^7', '[]'),
 ('moderator+', 'Modérateur+', '^7', '[]'),
-('owner', 'Propriétaire', '^7', '{\"commands.setjob\",\"commands.giveitem\", \"commands.giveweapon\", \"commands.clearloadout\", \"commands.givemoney\", \"commands.setgroup\", \"menu.noclip\"}'),
+('owner', 'Propriétaire', '^7', '{\"commands.announce\", \"commands.setjobpos\", \"commands.setjob\",\"commands.car\", \"commands.giveitem\", \"commands.giveweapon\", \"commands.clearloadout\", \"commands.givemoney\", \"commands.setgroup\", \"commands.clearinventory\", \"menu.noclip\"}'),
 ('vip', 'VIP', '^7', '[]'),
 ('vip+', 'VIP+', '^7', '[]');
 
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `cards`
---
 ALTER TABLE `cards`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `inventories`
---
 ALTER TABLE `inventories`
   ADD PRIMARY KEY (`identifier`);
 
---
--- Index pour la table `items`
---
 ALTER TABLE `items`
   ADD PRIMARY KEY (`name`);
 
---
--- Index pour la table `jobs`
---
 ALTER TABLE `jobs`
   ADD PRIMARY KEY (`name`);
 
---
--- Index pour la table `players`
---
 ALTER TABLE `players`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `ranks`
---
 ALTER TABLE `ranks`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `cards`
---
 ALTER TABLE `cards`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
---
--- AUTO_INCREMENT pour la table `players`
---
 ALTER TABLE `players`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id unique du joueur (utilisé pour la boutique)', AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id unique du joueur (utilisé pour la boutique)', AUTO_INCREMENT=32;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
